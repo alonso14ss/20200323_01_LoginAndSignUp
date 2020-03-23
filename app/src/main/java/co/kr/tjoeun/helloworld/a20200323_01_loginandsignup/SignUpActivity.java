a76dfcfc00df35a4cb5d3686f177541564a7ec8a
alonso14ss <reiyan92@gmail.com>
2020-03-23 16:13:25 +0900

    회원가입 환영메시지 출력

--- app/src/main/java/co/kr/tjoeun/helloworld/a20200323_01_loginandsignup/SignUpActivity.java
+++ app/src/main/java/co/kr/tjoeun/helloworld/a20200323_01_loginandsignup/SignUpActivity.java
@@ -6,7 +6,9 @@ import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import co.kr.tjoeun.helloworld.a20200323_01_loginandsignup.databinding.ActivitySignUpBinding;
@@ -43,6 +45,40 @@ public class SignUpActivity extends BaseActivity {
                     @Override
                     public void onResponse(JSONObject json) {
                         Log.d("회원가입",json.toString());
+
+//                        응용 문제 : 회원가입에 성공? "이름" 님 환영합니다! 토스트 출력
+//                        실패? 서버에서 주는 message를 그대로 토스트로 출력
+
+                        try {
+                            final String message = json.getString("message");
+                            int code = json.getInt("code");
+
+                            if (code==200){
+                                JSONObject data = json.getJSONObject("data");
+                                JSONObject user = data.getJSONObject("user");
+                                final String name = user.getString("name");
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(mContext,String.format(("%s님! 환영합니다!"), name),Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                            }
+                            else{
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(mContext,message,Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
                     }
                 });
 
