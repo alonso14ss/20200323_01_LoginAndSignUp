a1c89e992297c529a2b93b97944520d8c5d7cef5
alonso14ss <reiyan92@gmail.com>
2020-03-23 18:10:41 +0900

    사용자 목록 받아오기 테스트

--- app/src/main/java/co/kr/tjoeun/helloworld/a20200323_01_loginandsignup/MainActivity.java
+++ app/src/main/java/co/kr/tjoeun/helloworld/a20200323_01_loginandsignup/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -31,6 +32,13 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+
+        ServerUtil.getRequestUserList(mContext, "All", new ServerUtil.JsonResponseHandler() {
+            @Override
+            public void onResponse(JSONObject json) {
+                Log.d("사용자목록", json.toString());
+            }
+        });
 
 //        저장된 토큰을 확인해서 => 그 토큰으로 내 정보를 서버에서 다시 불러올것 - 조회 (get)
         ServerUtil.getRequestMyInfo(mContext, new ServerUtil.JsonResponseHandler() {
