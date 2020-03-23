e6d903186aa915a5476920931e6852199fda4403
alonso14ss <reiyan92@gmail.com>
2020-03-23 09:50:46 +0900

    로그인 화면 초안

---
+++ app/src/main/java/co/kr/tjoeun/helloworld/a20200323_01_loginandsignup/BaseActivity.java
@@ -0,0 +1,14 @@
+package co.kr.tjoeun.helloworld.a20200323_01_loginandsignup;
+
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public Context mContext = this;
+
+    public abstract void setupEvents;
+    public abstract void setValues;
+
+}
---
+++ app/src/main/java/co/kr/tjoeun/helloworld/a20200323_01_loginandsignup/MainActivity.java
@@ -0,0 +1,19 @@
+package co.kr.tjoeun.helloworld.a20200323_01_loginandsignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import co.kr.tjoeun.helloworld.a20200323_01_loginandsignup.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+    }
+}
