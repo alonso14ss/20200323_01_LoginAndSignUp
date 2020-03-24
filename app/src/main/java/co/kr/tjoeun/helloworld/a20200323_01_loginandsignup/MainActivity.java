72b4e323d69f256444bd6c125a10489cba431a5a
alonso14ss <reiyan92@gmail.com>
2020-03-24 15:07:18 +0900

    로그아웃 처리 구현

--- app/src/main/java/co/kr/tjoeun/helloworld/a20200323_01_loginandsignup/MainActivity.java
+++ app/src/main/java/co/kr/tjoeun/helloworld/a20200323_01_loginandsignup/MainActivity.java
@@ -1,16 +1,21 @@
 package co.kr.tjoeun.helloworld.a20200323_01_loginandsignup;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import co.kr.tjoeun.helloworld.a20200323_01_loginandsignup.databinding.ActivityMainBinding;
 import co.kr.tjoeun.helloworld.a20200323_01_loginandsignup.datas.User;
+import co.kr.tjoeun.helloworld.a20200323_01_loginandsignup.utils.ContextUtil;
 import co.kr.tjoeun.helloworld.a20200323_01_loginandsignup.utils.ServerUtil;
 
 public class MainActivity extends BaseActivity {
@@ -28,17 +33,36 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setupEvents() {
 
+        binding.logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                얼럿으로 정말 로그아웃 하시겠습니까?
+                AlertDialog.Builder alert = new AlertDialog.Builder(mContext);
+                alert.setMessage("정말 로그아웃 하시겠습니까?");
+                alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+//                        로그아웃 처리
+//                        저장된 토큰을 => 날려버림
+                        ContextUtil.setUserToken(mContext,"");
+//                        로그인 액티비티를 띄우자
+                        Intent intent = new Intent(mContext,LoginActivity.class);
+                        startActivity(intent);
+//                        메인 액티비티 종료
+                        finish();
+                    }
+                });
+                alert.setNegativeButton("취소",null);
+                alert.show();
+
+            }
+        });
+
     }
 
     @Override
     public void setValues() {
-
-        ServerUtil.getRequestUserList(mContext, "All", new ServerUtil.JsonResponseHandler() {
-            @Override
-            public void onResponse(JSONObject json) {
-                Log.d("사용자목록", json.toString());
-            }
-        });
 
 //        저장된 토큰을 확인해서 => 그 토큰으로 내 정보를 서버에서 다시 불러올것 - 조회 (get)
         ServerUtil.getRequestMyInfo(mContext, new ServerUtil.JsonResponseHandler() {
--- app/src/main/java/co/kr/tjoeun/helloworld/a20200323_01_loginandsignup/SplashActivity.java
+++ app/src/main/java/co/kr/tjoeun/helloworld/a20200323_01_loginandsignup/SplashActivity.java
@@ -3,9 +3,12 @@ package co.kr.tjoeun.helloworld.a20200323_01_loginandsignup;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import co.kr.tjoeun.helloworld.a20200323_01_loginandsignup.databinding.ActivitySplashBinding;
+import co.kr.tjoeun.helloworld.a20200323_01_loginandsignup.utils.ContextUtil;
 
 public class SplashActivity extends BaseActivity {
 
@@ -30,7 +33,27 @@ public class SplashActivity extends BaseActivity {
 //        토큰이 저장되어 있고 & 자동로그인이 체크되어 있다면
 //        => 메인 액티비티로 이동
 
-//        그렇지 않다면 => 로그인 액티비티로
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (!ContextUtil.getUserToken(mContext).equals("") && ContextUtil.isAutoLoginCheck(mContext)){
+                    Intent intent = new Intent(mContext, MainActivity.class);
+                    startActivity(intent);
+
+                }
+                //        그렇지 않다면 => 로그인 액티비티로
+                else {
+                    Intent intent = new Intent(mContext, LoginActivity.class);
+                    startActivity(intent);
+                }
+                finish();
+
+            }
+        },3000);
+
+
+
 
     }
 }
